e31de36c8be0fa0e2425420d21140d5950ce3c28
Daniele4299 <bonadonnadaniele@gmail.com>
2025-10-07 20:31:08 +0200

    Initial refactor

--- src/main/java/com/db/ayce/be/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.db.ayce.be.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String username;
-    private String password;
-}
--- src/main/java/com/db/ayce/be/service/CustomUserDetailsService.java
+++
@@ -1,7 +0,0 @@
-package com.db.ayce.be.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface CustomUserDetailsService extends UserDetailsService {
-    
-}
